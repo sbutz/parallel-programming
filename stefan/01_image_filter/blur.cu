8e528b25563bffc00af127ff966d252525240f39
Stefan Butz <stefan260699@gmail.com>
2024-10-19 10:37:09 +0000

    Unify blur programs
    A1.2 finished

--- stefan/01_image_filter/blur.cu
+++ stefan/01_image_filter/blur.cu
@@ -1,12 +1,15 @@
 #include "jpeg.h"
 #include "util.h"
 #include <cstdint>
+#include <cstdlib>
 #include <cuda.h>
 #include <iostream>
+
 
 __global__ void Blur(unsigned char* inputImage, unsigned char* outputImage, int width, int height, int channels, int margin) {
     int x = blockIdx.x * blockDim.x + threadIdx.x;
     int y = blockIdx.y * blockDim.y + threadIdx.y;
+    int channel = threadIdx.z;
 
     if (x < width && y < height) {
         int startX = max(x - margin, 0);
@@ -14,41 +17,35 @@ __global__ void Blur(unsigned char* inputImage, unsigned char* outputImage, int 
         int startY = max(y - margin, 0);
         int endY = min(y + margin, height);
 
-        float r = 0;
-        float g = 0;
-        float b = 0;
+        float v = 0;
         for (int i = startY; i < endY; i++)
         {
             for (int j = startX; j < endX; j++)
             {
-                r += inputImage[(i * width + j) * channels + 0];
-                g += inputImage[(i * width + j) * channels + 1];
-                b += inputImage[(i * width + j) * channels + 2];
+                v += inputImage[(i * width + j) * channels + channel];
             }
         }
         float n = (endX - startX) * (endY - startY);
-        outputImage[(y * width + x) * channels + 0] = r / n;
-        outputImage[(y * width + x) * channels + 1] = g / n;
-        outputImage[(y * width + x) * channels + 2] = b / n;
+        outputImage[(y * width + x) * channels + channel] = v / n;
     }
 }
 
 int main(int argc, char* argv[])
 {
-    if (argc != 3)
+    if (argc != 4)
     {
-        std::cerr << "Usage: " << argv[0] << " input.jpg output.jpg" << std::endl;
+        std::cerr << "Usage: " << argv[0] << " margin input.jpg output.jpg" << std::endl;
         return 1;
     }
 
-    const char* inputFilename = argv[1];
-    const char* outputFilename = argv[2];
+    int margin = std::atoi(argv[1]);
+    const char* inputFilename = argv[2];
+    const char* outputFilename = argv[3];
 
     auto hInputImage = Jpeg::FromFile(inputFilename);
     auto height = hInputImage.GetHeight();
     auto width = hInputImage.GetWidth();
     auto channels = hInputImage.GetChannels();
-    Assert(channels == 3, "Expecting rgb image");
 
     unsigned char *dInputImage, *dOutputImage;
     GpuAssert(cudaMalloc((void**)&dInputImage, width * height * channels * sizeof(float)));
@@ -56,9 +53,8 @@ int main(int argc, char* argv[])
 
     GpuAssert(cudaMemcpy(dInputImage, hInputImage.GetRawData(), width * height * channels, cudaMemcpyHostToDevice));
 
-    dim3 blockSize(16, 16);
+    dim3 blockSize(16, 16, channels);
     dim3 gridSize((width + blockSize.x - 1) / blockSize.x, (height + blockSize.y - 1) / blockSize.y);
-    int margin = 8;
 
     Blur<<<gridSize, blockSize>>>(dInputImage, dOutputImage, width, height, channels, margin);
     GpuAssert(cudaGetLastError());
