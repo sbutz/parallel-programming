44765ea7739e4f86c5a9101c55d8d69d72cbd3e7
Johannes Gerd Becker <jgbecker.www@gmail.com>
2025-01-05 14:56:30 +0100

    Revert "Removed personal folder from master branch"
    This reverts commit 9c2f0cbdc40285ae204775106a1a47b816e8b62d.

---
+++ 000_johannes/03_amd/perform_measurements.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+import os
+from datetime import datetime
+import subprocess
+
+MIN_SZ = 2 ** 3
+MAX_SZ = 2 ** 32 # 4 GiB
+N_RUNS = 100
+INPUT_FILE_PATH = os.path.join('.', 'input_data', 'test.txt')
+
+jsonDirectory = os.path.join('.', 'measurements')
+
+os.makedirs(jsonDirectory, exist_ok=True)
+
+now = datetime.now()
+timestamp = now.strftime('%Y%m%d-%H%M%S') + f"{now.microsecond // 1000:03d}"
+outDirectory = os.path.join(jsonDirectory, f'{timestamp}-amd')
+
+os.mkdir(outDirectory)
+
+deviceQueryCmd = os.path.join('.', 'bin', 'deviceQuery_hip')
+result = subprocess.run(
+    [deviceQueryCmd], stdout=subprocess.PIPE, universal_newlines=True, check=True
+)
+with open(os.path.join(outDirectory, f'{timestamp}-devicequery.txt'), 'w') as file:
+    file.write(result.stdout)
+    
+idx = 0
+cmd = os.path.join('.', 'bin', 'histogram_hip')
+
+for arg in ['oasg', 'oasgl']:
+    result = subprocess.run(
+        [cmd, INPUT_FILE_PATH, arg, f'{N_RUNS:0.0f}'],
+        stdout=subprocess.PIPE, universal_newlines=True, check=True
+    )
+    with open(os.path.join(outDirectory, f'{timestamp}-{idx:03.0f}.json'), 'w') as file:
+        file.write(result.stdout)
+    idx += 1
+
+def generateSzArgsLetterCombinations():
+    szArgsCombinations = []
+    sz = MIN_SZ
+    while sz <= MAX_SZ:
+        args = 'sg'
+        if sz <= 2 ** 31:
+            args += "oa"
+        szArgsCombinations.append([f'{sz}', args])
+        szArgsCombinations.append([f'{sz}', args + 'u'])
+        szArgsCombinations.append([f'{sz}', args + 'l'])
+        szArgsCombinations.append([f'{sz}', args + 'ul'])
+        sz *= 2
+    return szArgsCombinations
+
+for sz, args in generateSzArgsLetterCombinations():
+    result = subprocess.run(
+        [cmd, '--', f'{sz}', args, f'{N_RUNS:0.0f}'],
+        stdout=subprocess.PIPE, universal_newlines=True, check=True
+    )
+    with open(os.path.join(outDirectory, f'{timestamp}-{idx:03.0f}.json'), 'w') as file:
+        file.write(result.stdout)
+    idx += 1
