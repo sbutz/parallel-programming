1eeb7d369e6a937eaf29d7610356efdb2987e204
sihlfall <jaybee@sihlfall.ch>
2025-01-19 18:36:26 +0100

    Intermediate

--- 000_johannes/dbscan/experiments/bfs/src/bfs.cu
+++ 000_johannes/dbscan/experiments/bfs/src/bfs.cu
@@ -33,7 +33,8 @@ __device__ void appendToFrontier(Frontier * frontier, IdxType vertex) {
 
 __global__ void kernel_bfs(
     Graph graph,
-    bool * d_visited,   // working memory
+    bool * d_visited,
+    unsigned int visitedTag, // must be != 0
     Frontier * frontier,
     Frontier * newFrontier
 ) {
@@ -49,7 +50,7 @@ __global__ void kernel_bfs(
             for (IdxType j = incidenceListStart; j < incidenceListEnd; ++j) {
                 IdxType destination = graph.destinations[j];
                 if(!d_visited[destination]) {
-                    d_visited[destination] = true;
+                    d_visited[destination] = visitedTag;
                     appendToFrontier(newFrontier, destination);
                 }
             }
@@ -58,21 +59,22 @@ __global__ void kernel_bfs(
     }
 }
 
-IdxType sampleGraphNVertices       = 5;
-auto sampleGraphIncidenceLists = std::array<IdxType, 7>  { 0,    2,    4,       7, 7, 8,    10 }; // has length nVertices + 1 to simplify code
-auto sampleGraphDestinations   = std::array<IdxType, 10> { 1, 3, 4, 5, 1, 3, 5,    3, 4, 5 };
+constexpr IdxType sampleGraphNVertices       = 5;
+constexpr auto sampleGraphIncidenceLists = std::array<IdxType, 7>  { 0,    2,    4,       7, 7, 8,    10 }; // has length nVertices + 1 to simplify code
+constexpr auto sampleGraphDestinations   = std::array<IdxType, 10> { 1, 3, 4, 5, 1, 3, 5,    3, 4, 5 };
 
-auto tmpFrontierData = std::array<IdxType, 100> {};
+auto tmpFrontierData = std::array<IdxType, sampleGraphDestinations.size()> {};
+
 int main () {
     constexpr int threadsPerBlock = 128;
 
     DeviceVector<IdxType> d_sampleGraphInicidenceLists(sampleGraphIncidenceLists);
     DeviceVector<IdxType> d_sampleGraphDestinations(sampleGraphDestinations);
     DeviceVector<bool> d_visited (sampleGraphNVertices);
-    DeviceVector<IdxType> d_frontierData (UninitializedDeviceVectorTag {}, 4 * sampleGraphNVertices);
+    DeviceVector<IdxType> d_frontierData (UninitializedDeviceVectorTag {}, 2 * sampleGraphDestinations.size());
     DeviceVector<Frontier> d_frontiers (std::array<Frontier, 2> {
         Frontier { 1, d_frontierData.data() },
-        Frontier { 0, d_frontierData.data() + 2 * sampleGraphNVertices }
+        Frontier { 0, d_frontierData.data() + sampleGraphDestinations.size() }
     });
     IdxType startVertex = 0;
     CUDA_CHECK(cudaMemcpy(d_frontierData.data(), &startVertex, sizeof(IdxType), cudaMemcpyHostToDevice));
@@ -96,6 +98,7 @@ int main () {
         >>> (
             sampleGraph_d,
             d_visited.data(),
+            1,
             &d_frontiers.data()[currentFrontier],
             &d_frontiers.data()[!currentFrontier]
         );
