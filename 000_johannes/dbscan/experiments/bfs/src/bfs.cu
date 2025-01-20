02457b7747146291bb11dc54e606155f4aa03678
sihlfall <jaybee@sihlfall.ch>
2025-01-20 16:01:17 +0100

    Intermediate

--- 000_johannes/dbscan/experiments/bfs/src/bfs.cu
+++ 000_johannes/dbscan/experiments/bfs/src/bfs.cu
@@ -90,15 +90,16 @@ struct ComponentFinder {
     template <typename Callback>
     void findComponent(
         Graph const * graph, IdxType startVertex, IdxType visitedTag,
-        Callback callback = []{}
+        Callback && callback = []{}
     ) {
         constexpr int threadsPerBlock = 128;
         IdxType startValues [2] = { 1, startVertex };
 
         CUDA_CHECK(cudaMemcpy(this->frontiers[this->currentFrontier].d_cntFrontier, &startValues, 2 * sizeof(IdxType), cudaMemcpyHostToDevice))
+        CUDA_CHECK(cudaMemset(&this->d_visited[startVertex], visitedTag, sizeof(IdxType)))
         for (;;) {
             CUDA_CHECK(cudaMemset(this->frontiers[!this->currentFrontier].d_cntFrontier, 0, sizeof(IdxType)))
-            kernel_bfs<<<
+            kernel_bfs <<<
                 dim3((graph->nVertices + threadsPerBlock - 1) / threadsPerBlock),
                 dim3(threadsPerBlock)
             >>> (
@@ -112,7 +113,7 @@ struct ComponentFinder {
             );
             cudaDeviceSynchronize();
 
-            callback();
+            std::forward<Callback> (callback) ();
 
             IdxType cntNewFrontier;
             CUDA_CHECK(cudaMemcpy(
@@ -125,7 +126,70 @@ struct ComponentFinder {
     }
 };
 
+__global__ void kernel_findUnvisited(
+    IdxType * outBuffer,
+    IdxType * d_visited,
+    IdxType nVertices,
+    IdxType startPos
+) {
+    unsigned int tid = blockDim.x * blockIdx.x + threadIdx.x;
 
+    // TODO: This is bad. We should start at startPos and finish early when vertex has been found.
+    if (tid < nVertices) {
+        if (!d_visited[tid]) {
+            outBuffer[0] = 1; // true
+            outBuffer[1] = tid;
+        }
+    }    
+}
+
+struct AllComponentsFinder {
+    ComponentFinder cf;
+    IdxType nextFreeTag;
+    IdxType nextStartIndex;
+    IdxType * d_resultBuffer;
+
+    AllComponentsFinder(Graph const * graph, std::size_t maxFrontierSize)
+    : cf (graph, maxFrontierSize), nextFreeTag(1), nextStartIndex(0) {
+        CUDA_CHECK(cudaMalloc(&this->d_resultBuffer, 2 * sizeof(IdxType)))
+    }
+
+    AllComponentsFinder(AllComponentsFinder const &) = delete;
+
+    ~AllComponentsFinder() {
+        (void)cudaFree(this->d_resultBuffer);
+    }
+
+    auto findNextUnvisited(Graph const * graph) {
+        constexpr int threadsPerBlock = 128;
+        CUDA_CHECK(cudaMemset(this->d_resultBuffer, 0, 2 * sizeof(IdxType)))
+
+        kernel_findUnvisited <<<
+            dim3((graph->nVertices + threadsPerBlock - 1) / threadsPerBlock),
+            dim3(threadsPerBlock)
+        >>> (this->d_resultBuffer, this->cf.d_visited, graph->nVertices, 0);
+        cudaDeviceSynchronize();
+
+        IdxType localBuffer [2];
+        CUDA_CHECK(cudaMemcpy(localBuffer, this->d_resultBuffer, 2 * sizeof(IdxType), cudaMemcpyDeviceToHost))
+
+        struct Result {
+            bool wasFound;
+            IdxType idx;
+        };
+        return Result{!!localBuffer[0], localBuffer[1]};
+    }
+
+    template <typename Callback>
+    void findAllComponents(Graph const * graph, Callback && callback = []{}) {
+        for (;;) {
+            auto nextUnvisited = this->findNextUnvisited(graph);
+            if (!nextUnvisited.wasFound) return;
+            this->cf.findComponent(graph, nextUnvisited.idx, this->nextFreeTag, std::forward<Callback> (callback));
+            ++this->nextFreeTag;
+        }
+    }
+};
 
 constexpr IdxType sampleGraphNVertices   = 5;
 constexpr auto sampleGraphIncidenceLists = std::array<IdxType, 7>  { 0,    2,    4,       7, 7, 8,    10 }; // has length nVertices + 1 to simplify code
@@ -144,19 +208,19 @@ int main () {
         d_sampleGraphDestinations.data()
     };
 
-    ComponentFinder cf(&sampleGraph_d, sampleGraphDestinations.size());
+    AllComponentsFinder acf(&sampleGraph_d, sampleGraphDestinations.size());
 
-    cf.findComponent(&sampleGraph_d, startVertex, 1, [&]() {
+    acf.findAllComponents(&sampleGraph_d, [&]() {
         IdxType cntNewFrontier = 0;
         CUDA_CHECK(cudaMemcpy(
             &cntNewFrontier,
-            cf.frontiers[!cf.currentFrontier].d_cntFrontier,
+            acf.cf.frontiers[!acf.cf.currentFrontier].d_cntFrontier,
             sizeof(IdxType),
             cudaMemcpyDeviceToHost
         ))
         CUDA_CHECK(cudaMemcpy(
             tmpFrontierData.data(),
-            cf.frontiers[!cf.currentFrontier].d_frontier,
+            acf.cf.frontiers[!acf.cf.currentFrontier].d_frontier,
             sampleGraphDestinations.size() * sizeof(IdxType),
             cudaMemcpyDeviceToHost
         ))
